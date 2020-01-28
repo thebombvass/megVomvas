a7553bb91aaa8891c8f61b7bfba0abcd07dd9653
thebombvass <mevomvas@bu.edu>
2020-01-28 11:23:56 -0500

    changes to nav bar

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    //Nav Links
+    let url = window.location.protocol +"//"+ window.location.hostname + ":"+window.location.port + window.location.pathname
+    console.log(url)
+
+    // let aboutMeLink = url
+
+    //Creating navbar here once
+    // $("header").html(
+    //     <ul class="nav">
+    //     <li class="nav-item">
+    //       <a class="nav-link" href="https://thebombvass.github.io/megVomvas/">About Me</a>
+    //     </li>
+    //     <li class="nav-item">
+    //       <a class="nav-link " href="https://thebombvass.github.io/megVomvas/resume.html">Professional Resume</a>
+    //     </li>
+    //     <li class="nav-item">
+    //       <a class="nav-link active" href="https://thebombvass.github.io/megVomvas/projects.html">Highlighted Projects</a>
+    //     </li>
+    //     <li class="nav-item">
+    //       <a class="nav-link" href="https://thebombvass.github.io/megVomvas/contact.html">Contact Me</a>
+    //     </li>
+    //     <li class="nav-item">
+    //       <a class="nav-link" href="#">Fun</a>
+    //     </li>
+    //   </ul>
+    // )
+
+  });
